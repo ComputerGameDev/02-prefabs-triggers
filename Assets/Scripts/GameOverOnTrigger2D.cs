2f07bb3b444624bfc0af074fe5bf69f8b465c846
Tomer Gozlan <tomgoz96@gmail.com>
2024-12-01 21:41:04 +0200

    Finish

--- Assets/Scripts/GameOverOnTrigger2D.cs
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -1,22 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverOnTrigger2D : MonoBehaviour
 {
     [Tooltip("Every object tagged with this tag will trigger game over")]
-    [SerializeField] string triggeringTag;
+    [SerializeField] private string triggeringTag;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
             Debug.Log("Game over!");
+
+            // Quit application in runtime builds
             Application.Quit();
-            UnityEditor.EditorApplication.isPlaying = false;  // Error on editor 2021.3
+
+            // Stop play mode in the Unity Editor
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
         }
     }
 
-    private void Update() {
+    private void Update()
+    {
         /* Just to show the enabled checkbox in Editor */
     }
-
 }
